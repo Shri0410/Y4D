4cf387584fa7cc17ec2d8bd8ba60a59c7cea45e2
VaradKau <varadkau@gmail.com>
2025-09-25 12:49:32 +0530

    Varad, dashboard rport update

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/api/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use('/api/board-trustees', require('./routes/boardTrustees'));
 
 // Routes
