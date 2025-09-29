7086ece48627db4997fb4e0b9e40474a36df93ee
Shri0410 <waghmareshrinivas007@gmail.com>
2025-09-29 18:45:31 +0530

    SHRI, add a donate now button for the home page and in the navbar dropdown buttons +- button and some alignment in mobile view

---
+++ frontend/src/component/DonateButton.jsx
@@ -0,0 +1,13 @@
+// src/components/DonateButton.jsx
+import React from "react";
+import "./DonateButton.css";
+
+const DonateButton = () => {
+  return (
+    <a href="/DonateNow" className="donate-btn">
+      Donate Now
+    </a>
+  );
+};
+
+export default DonateButton;
